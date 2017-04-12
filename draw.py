a22856095a837d500dc35740f36a8db9f7ecebab
Alvin Zhu <alvinzhu33@yahoo.com>
2017-04-12 12:44:56 -0400

    Stuck :(

--- draw.py
+++ draw.py
@@ -13,7 +13,7 @@ def draw_polygons( points, screen, color ):
         return
 
     point = 0
-    while point < len(points) - 1:
+    while point < len(points) - 2:
         if backcull(int(points[point][0]), int(points[point][1]), int(points[point][2]),
                     int(points[point+1][0]), int(points[point+1][1]), int(points[point+1][2]),
                     int(points[point+2][0]), int(points[point+2][1]), int(points[point+2][2])):
@@ -35,7 +35,7 @@ def backcull(x0, y0, z0, x1, y1, z1, x2, y2, z2):
     A = [x1-x0, y1-y0, z1-z0];
     B = [x2-x0, y2-y0, z2-z0]
     n = [A[1]*B[2] - A[2]*B[1], A[2]*B[0] - A[0]*B[2], A[0]*B[1] - A[1]*B[0]];
-    if(n[2] > 0):
+    if n[2] > 0:
         return True;
 
 
